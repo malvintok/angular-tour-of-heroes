f3f32c2617315e7eca6a970fe582822a98aa5552
Malvin Tok <93638561+malvintok@users.noreply.github.com>
2022-05-17 09:58:19 +0800

    Commit

---
+++ src/app/hero.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Hero } from "./hero";
+import { HEROES } from "./mock-heroes";
+import { Observable, of } from "rxjs";
+import { MessageService } from './message.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+  
+  constructor(private messageService: MessageService) { }
+
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES);
+    this.messageService.add('HeroService: Fetched heroes');
+    return heroes;
+  }
+
+  getHero(id: number) {
+    const hero = HEROES.find(h => h.id === id)!;
+    this.messageService.add(`HeroService: Fetched hero id = ${id}`);
+    return of(hero);
+  }
+
+}
